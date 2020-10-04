61c0648f43a5b24e7d31e9db980419a2cf258c10
James Goff <james_goff88@outlook.com>
2020-10-04 17:05:33 -0500

    add contact form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -30,7 +30,7 @@ const ContactState = (props) => {
 				type: 'personal'
 			},
 			{
-				id: 2,
+				id: 3,
 				name: 'Ted Smith',
 				email: 'ted@yahoo.com',
 				phone: '333-333-3333',
@@ -39,11 +39,24 @@ const ContactState = (props) => {
 		]
 	};
 
-	const [ state, disptach ] = useReducer(contactReducer, initialState);
+	const [ state, dispatch ] = useReducer(contactReducer, initialState);
 
 	// Add Contact
+	const addContact = (contact) => {
+		contact.id = uuidv4();
+		dispatch({
+			type: ADD_CONTACT,
+			payload: contact
+		});
+	};
 
 	// Delete Contact
+	const deleteContact = (id) => {
+		dispatch({
+			type: DELETE_CONTACT,
+			payload: id
+		});
+	};
 
 	// Set Current Contact
 
@@ -58,7 +71,9 @@ const ContactState = (props) => {
 	return (
 		<ContactContext.Provider
 			value={{
-				contacts: state.contacts
+				contacts: state.contacts,
+				addContact,
+				deleteContact
 			}}
 		>
 			{props.children}
