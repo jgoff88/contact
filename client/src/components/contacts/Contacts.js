08b1f2af73296299f619f89a5c4b42c576118b6e
James Goff <james_goff88@outlook.com>
2020-10-05 15:58:18 -0500

    filter and clear contacts functionality

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,13 +1,36 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactsContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
 	const contactContext = useContext(ContactsContext);
 
-	const { contacts } = contactContext;
+	const { contacts, filtered } = contactContext;
 
-	return <Fragment>{contacts.map((contact) => <ContactItem key={contact.id} contact={contact} />)}</Fragment>;
+	if (contacts.length === 0) {
+		return <h3>Please add a contact</h3>;
+	}
+
+	return (
+		<Fragment>
+			<TransitionGroup>
+				{filtered !== null ? (
+					filtered.map((contact) => (
+						<CSSTransition key={contact.id} timeout={500} classNames="item">
+							<ContactItem contact={contact} />
+						</CSSTransition>
+					))
+				) : (
+					contacts.map((contact) => (
+						<CSSTransition key={contact.id} timeout={500} classNames="item">
+							<ContactItem contact={contact} />
+						</CSSTransition>
+					))
+				)}
+			</TransitionGroup>
+		</Fragment>
+	);
 };
 
 export default Contacts;
