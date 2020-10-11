8d8a26ab11ad1553c14bcfa88b2fb1e84c96b69d
James Goff <james_goff88@outlook.com>
2020-10-10 21:06:31 -0500

    connect register from to mongodb

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,17 +13,24 @@ const Navbar = ({ title, icon }) => {
 				<li>
 					<Link to="/about">About</Link>
 				</li>
+				<li>
+					<Link to="/register">Register</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
 			</ul>
 		</div>
 	);
 };
+
 Navbar.propTypes = {
 	title: PropTypes.string.isRequired,
 	icon: PropTypes.string
 };
 
 Navbar.defaultProps = {
-	title: 'Contact Keeper',
+	title: ' Contact Keeper',
 	icon: 'fas fa-id-card-alt'
 };
 export default Navbar;
